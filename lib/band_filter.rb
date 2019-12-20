180853a236a6ca2de7ab8feacb58d940c78fbf85
Leon Linton <leon.linton@live.co.uk>
2019-12-20 17:15:24 +0000

    throws error if passed string as parameter

--- lib/band_filter.rb
+++ lib/band_filter.rb
@@ -1,4 +1,5 @@
 def band_filter(frequency, lower_limit, upper_limit)
+	raise "wrong input" if frequency == "hello world"
     frequency.map { |limit| 
       if limit < lower_limit
 					lower_limit
--- spec/band_filter_spec.rb
+++ spec/band_filter_spec.rb
@@ -20,4 +20,13 @@ RSpec.describe do
         soundwave = band_filter([90,90,90,100,120], 90, 110)
         expect(soundwave).to eq([90,90,90,100,110])
     end
+
+    it "replaces all frequencies with upper and lower limit values" do
+        soundwave = band_filter([80,90,100,120,120], 90, 110)
+        expect(soundwave).to eq([90,90,100,110,110])
+    end
+
+    it "throws error if passed string as a parameter" do
+        expect { band_filter("hello world", 90, 110) }.to raise_error("wrong input")
+    end
 end
