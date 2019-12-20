3c42a209771cea503f1ad92506f757d8324642ea
Leon Linton <leon.linton@live.co.uk>
2019-12-20 17:07:22 +0000

    Replaces high frequencies with upper limit value

--- lib/band_filter.rb
+++ lib/band_filter.rb
@@ -1,7 +1,9 @@
-def band_filter(frequency, lower_limit)
+def band_filter(frequency, lower_limit, upper_limit)
     frequency.map { |limit| 
       if limit < lower_limit
 					lower_limit
+			elsif limit > upper_limit
+				upper_limit
 			else
 				limit
 			end        
--- spec/band_filter_spec.rb
+++ spec/band_filter_spec.rb
@@ -2,17 +2,22 @@ require 'band_filter'
 
 RSpec.describe do
     it "responds with an array" do
-        soundwave = band_filter([90,90,90,100,100], 90)
+        soundwave = band_filter([90,90,90,100,100], 90, 100)
         expect(soundwave).to be_an_instance_of(Array)
     end
 
     it "when passed frequency, returns same frequency" do
-       soundwave = band_filter([90,90,90,100,100], 90)
+       soundwave = band_filter([90,90,90,100,100], 90, 100)
        expect(soundwave).to eq([90,90,90,100,100])
     end
 
-    it "replaces low frequencies with lower limit value " do
-        soundwave = band_filter([80,90,90,100,100], 90)
+    it "replaces low frequencies with lower limit value" do
+        soundwave = band_filter([80,90,90,100,100], 90, 100)
         expect(soundwave).to eq([90,90,90,100,100])
+    end
+
+    it "replaces high frequencies with upper limit value" do
+        soundwave = band_filter([90,90,90,100,120], 90, 110)
+        expect(soundwave).to eq([90,90,90,100,110])
     end
 end
